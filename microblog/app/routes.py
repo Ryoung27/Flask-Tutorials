f9420c3f7f6df996a39f6bdfb5cf27fcb77fc16a
Ryoung27 <RRYoung89@gmail.com>
2019-09-29 11:59:30 -0500

    register

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,12 +2,12 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Richie'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -18,7 +18,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,3 +29,19 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
